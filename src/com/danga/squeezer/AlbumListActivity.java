6b4057fda9e35d85cfb604faa90a3bbf3a081ba3
Nik Clayton <nik@ngo.org.uk>
2011-06-04 14:25:52 +0200

    Whitespace sync.

--- src/com/danga/squeezer/AlbumListActivity.java
+++ src/com/danga/squeezer/AlbumListActivity.java
@@ -1,3 +1,4 @@
+
 package com.danga.squeezer;
 
 import android.app.ListActivity;
@@ -5,7 +6,6 @@ import android.content.ContentProviderClient;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Build;
 import android.os.Bundle;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
@@ -15,85 +15,90 @@ import com.danga.squeezer.service.AlbumCache.Albums;
 import com.danga.squeezer.service.AlbumCacheCursor;
 
 public class AlbumListActivity extends ListActivity implements AbsListView.OnScrollListener {
-	private static final String TAG = AlbumListActivity.class.getName();
-	private static Bundle LiveUpdateT = new Bundle();
-	private static Bundle LiveUpdateF = new Bundle();
-	
-	private ContentProviderClient mContentProviderClient = null;
-	private Cursor cursor = null;
-	
+    private static final String TAG = AlbumListActivity.class.getName();
+    private static Bundle LiveUpdateT = new Bundle();
+    private static Bundle LiveUpdateF = new Bundle();
 
-	// Columns to bind
-	private static final String[] from = new String[] { Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH };
-	private static final int[] to = new int[] { R.id.text1, R.id.text2, R.id.icon };
+    private ContentProviderClient mContentProviderClient = null;
+    private Cursor cursor = null;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.album_list_activity);
+    // Columns to bind
+    private static final String[] from = new String[] {
+            Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH
+    };
 
-		cursor = managedQuery(Albums.CONTENT_URI,
-				new String[] { Albums._ID, Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH },
-				null, null, null);
-		
-		SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this,
-				R.layout.album_list_entry, cursor, from, to);
+    private static final int[] to = new int[] {
+            R.id.text1, R.id.text2, R.id.icon
+    };
 
-		LiveUpdateT.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
-		LiveUpdateT.putBoolean("LiveUpdate", true);
-		LiveUpdateF.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
-		LiveUpdateF.putBoolean("LiveUpdate", false);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.album_list_activity);
 
-		setListAdapter(mAdapter);
-	
-		getListView().setOnScrollListener(this);
-	}
-	
-	@Override
-	public void onResume() {
-		super.onResume();	
-		mContentProviderClient =
-			getContentResolver().acquireContentProviderClient(Albums.CONTENT_URI);
-	}
-	
-	@Override
-	public void onPause() {
-		super.onPause();
-		if (mContentProviderClient != null) {
-			mContentProviderClient.release();
-		}
-	}
-	
-	static void show(Context context) {
+        cursor = managedQuery(Albums.CONTENT_URI,
+                new String[] {
+                        Albums._ID, Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH
+                },
+                null, null, null);
+
+        SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this,
+                R.layout.album_list_entry, cursor, from, to);
+
+        LiveUpdateT.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
+        LiveUpdateT.putBoolean("LiveUpdate", true);
+        LiveUpdateF.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
+        LiveUpdateF.putBoolean("LiveUpdate", false);
+
+        setListAdapter(mAdapter);
+
+        getListView().setOnScrollListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mContentProviderClient =
+                getContentResolver().acquireContentProviderClient(Albums.CONTENT_URI);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (mContentProviderClient != null)
+            mContentProviderClient.release();
+    }
+
+    static void show(Context context) {
         final Intent intent = new Intent(context, AlbumListActivity.class);
         context.startActivity(intent);
     }
-	
-	/** OnScrollListener methods */
-	
+
+    /** OnScrollListener methods */
+
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
             int totalItemCount) {
     }
-	
+
     public void onScrollStateChanged(AbsListView view, int scrollState) {
         switch (scrollState) {
-        case OnScrollListener.SCROLL_STATE_IDLE:
-            cursor.respond(LiveUpdateT);
-            
-            Bundle extras = new Bundle();
-            extras.putInt("TYPE", AlbumCacheCursor.TYPE_REQUEST_PAGE);
-            extras.putInt("firstPosition", view.getFirstVisiblePosition());
-            extras.putInt("lastPosition", view.getLastVisiblePosition());
-            cursor.respond(extras);
+            case OnScrollListener.SCROLL_STATE_IDLE:
+                cursor.respond(LiveUpdateT);
 
-            break;
-        
-        case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
-            break;
+                Bundle extras = new Bundle();
+                extras.putInt("TYPE", AlbumCacheCursor.TYPE_REQUEST_PAGE);
+                extras.putInt("firstPosition", view.getFirstVisiblePosition());
+                extras.putInt("lastPosition", view.getLastVisiblePosition());
+                cursor.respond(extras);
 
-        case OnScrollListener.SCROLL_STATE_FLING:
-            cursor.respond(LiveUpdateF);
+                break;
+
+            case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
+                break;
+
+            case OnScrollListener.SCROLL_STATE_FLING:
+                cursor.respond(LiveUpdateF);
         }
     }
-	
+
 }
