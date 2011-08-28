fb9082486aa7a06ea68d8c43f5955bd7b832c568
Nik Clayton <nik@ngo.org.uk>
2011-08-28 14:06:19 +0200

    Use the Honeycomb Fragment API (as implemented by the v4 compatability library) and split the album list activity in to the activity and an album list fragment.
    Apart from making it easier to target this at devices with
    larger screens, it uses the new CursorLoader API so that
    the Cursor IO work happens off the UI thread, which makes
    scrolling through a long list of albums feel a lot smoother
    when the cache provider is fetching data at the same time.

--- src/com/danga/squeezer/AlbumsListActivity.java
+++ src/com/danga/squeezer/AlbumsListActivity.java
@@ -4,71 +4,49 @@ package com.danga.squeezer;
 import java.util.List;
 
 import android.app.Dialog;
-import android.app.ListActivity;
 import android.app.ProgressDialog;
 import android.content.ComponentName;
 import android.content.ContentProviderClient;
-import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.RemoteException;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AbsListView.OnScrollListener;
-import android.widget.AdapterView;
-import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
 
 import com.danga.squeezer.itemlists.IServiceAlbumListCallback;
 import com.danga.squeezer.model.SqueezerAlbum;
-import com.danga.squeezer.service.AlbumCache;
 import com.danga.squeezer.service.AlbumCache.Albums;
-import com.danga.squeezer.service.AlbumCacheCursor;
 import com.danga.squeezer.service.AlbumCacheProvider;
 import com.danga.squeezer.service.ISqueezeService;
 import com.danga.squeezer.service.SqueezeService;
 import com.danga.squeezer.service.SqueezerServerState;
 
-public class AlbumsListActivity extends ListActivity implements AbsListView.OnScrollListener,
-        AbsListView.OnItemClickListener {
+public class AlbumsListActivity extends FragmentActivity implements
+        AlbumsListFragment.OnAlbumSelectedListener {
     @SuppressWarnings("unused")
     private static final String TAG = AlbumsListActivity.class.getName();
-    private static Bundle LiveUpdateT = new Bundle();
-    private static Bundle LiveUpdateF = new Bundle();
-
-    private ListView mListView = null;
-
-    private ContentProviderClient mContentProviderClient;
-    private AlbumCacheProvider mProvider;
-    private Cursor mCursor;
 
     /** The number of items per page. */
     private final int mPageSize = Squeezer.getContext().getResources()
             .getInteger(R.integer.PageSize);
 
-    // Columns to bind to resources (in order)
-    private static final String[] from = new String[] {
-            Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH
-    };
-
-    // Resources to bind column values to (in order)
-    private static final int[] to = new int[] {
-            R.id.text1, R.id.text2, R.id.icon
-    };
-
     private static final int PROGRESS_DIALOG = 0;
     private ProgressDialog progressDialog;
+
+    private AlbumCacheProvider mProvider;
+    private ContentProviderClient mContentProviderClient;
 
     private ISqueezeService mService;
     private final ServiceConnection serviceConnection = new ServiceConnection() {
@@ -85,54 +63,28 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.albums_list_activity);
-
-        mListView = getListView();
-        mListView.setOnItemClickListener(this);
-        mListView.setOnScrollListener(this);
-
-        resetListAdapter();
-
-        LiveUpdateT.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
-        LiveUpdateT.putBoolean("LiveUpdate", true);
-        LiveUpdateF.putInt("TYPE", AlbumCacheCursor.TYPE_LIVEUPDATE);
-        LiveUpdateF.putBoolean("LiveUpdate", false);
-
-    }
-
-    /**
-     *
-     */
-    private void resetListAdapter() {
-        mCursor = managedQuery(Albums.CONTENT_URI,
-                new String[] {
-                        Albums._ID, Albums.COL_NAME, Albums.COL_ARTIST, Albums.COL_ARTWORK_PATH
-                },
-                null, null, null);
-
-        setTitle("Albums: " + mCursor.getCount());
-
-        SimpleCursorAdapter mAdapter = new SimpleCursorAdapter(this,
-                R.layout.albums_list_entry, mCursor, from, to);
-
-        setListAdapter(mAdapter);
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
-        mContentProviderClient =
-                getContentResolver().acquireContentProviderClient(Albums.CONTENT_URI);
+
+        // Get a reference to the content provider.
+        mContentProviderClient = getContentResolver().acquireContentProviderClient(
+                Albums.CONTENT_URI);
         mProvider = (AlbumCacheProvider) mContentProviderClient.getLocalContentProvider();
 
+        // Bind the service.
         Intent intent = new Intent(this, SqueezeService.class);
         bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
     }
 
     @Override
-    public void onPause() {
+    protected void onPause() {
         super.onPause();
-        if (mContentProviderClient != null)
-            mContentProviderClient.release();
+
+        mContentProviderClient = null;
+        mProvider = null;
 
         try {
             mService.unregisterAlbumListCallback(albumListCallback);
@@ -145,10 +97,7 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
             unbindService(serviceConnection);
     }
 
-    /*
-     * (non-Javadoc)
-     * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
-     */
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
@@ -157,47 +106,71 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
         return true;
     }
 
-    /*
-     * (non-Javadoc)
+    /** OnAlbumSelectedListener methods. */
+
+    public void onAlbumSelected(Uri albumUri) {
+        Intent i = new Intent(Intent.ACTION_VIEW, albumUri);
+        startActivity(i);
+    }
+
+    /**
+     * Process the options menu.
+     *
      * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
      */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.list);
+
         switch (item.getItemId()) {
             case R.id.clear_cache:
+                /**
+                 * Remove the list fragment, clear the cache, and re-add the
+                 * list fragment.
+                 */
                 if (mProvider != null) {
-                    mCursor.close();
+                    FragmentTransaction t = fragmentManager.beginTransaction();
+                    t.remove(fragment);
+                    t.commit();
+
                     mProvider.rebuildCache();
-                    resetListAdapter();
+
+                    t = fragmentManager.beginTransaction();
+                    t.add(R.id.list, new AlbumsListFragment());
+                    t.commit();
                 }
 
                 return true;
 
-                /**
-                 * Progress dialog approach. Set up a callback to catch new
-                 * albums when they arrive. The callback posts messages to a
-                 * handler. The handler updates the state of the progress dialog
-                 * on each message, and requests the next set of albums from the
-                 * server.
-                 */
             case R.id.preload_cache:
-                if (mProvider != null) {
-                    try {
-                        mService.registerAlbumListCallback(albumListCallback);
-                        mCursor.close();
-                        showDialog(PROGRESS_DIALOG);
-                        mService.albums(0, "album", null, null, null, null);
+                /**
+                 * Set up a callback to catch new albums when they arrive. The
+                 * callback posts messages to a handler. The handler updates the
+                 * state of the progress dialog on each message, and requests
+                 * the next set of albums from the server.
+                 */
+                try {
+                    // Remove the album list fragment, so it doesn't try and
+                    // update while the cache is loaded.
+                    FragmentTransaction t = fragmentManager.beginTransaction();
+                    t.remove(fragment);
+                    t.commit();
 
-                        /*
-                         * The other half of this (resetting the list adapter,
-                         * turning off the dialog box, etc) is in
-                         * mProgressHandler.
-                         */
-                    } catch (RemoteException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
+                    // Register the callback, enable the dialog, and start
+                    // fetching albums.
+                    mService.registerAlbumListCallback(albumListCallback);
+                    showDialog(PROGRESS_DIALOG);
+                    mService.albums(0, "album", null, null, null, null);
+                } catch (RemoteException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
                 }
+
+                /*
+                 * The other half of this (re-adding the fragment, turning off
+                 * the dialog box, etc) is in mProgressHandler.
+                 */
                 return true;
         }
 
@@ -205,22 +178,31 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
     }
 
     /**
-     * A handler for messages posted by albumListCallback.
+     * Handle messages posted by albumListCallback.
+     * <p>
+     * Either request the next set of albums if there are more to come, or
+     * re-add the list fragment, clean up the dialog, and remove the
+     * albumListCallback.
      */
     final Handler mProgressHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             int total = msg.arg1;
             progressDialog.setProgress(total);
+
             if (total >= progressDialog.getMax()) {
                 dismissDialog(PROGRESS_DIALOG);
+
+                FragmentTransaction t = getSupportFragmentManager().beginTransaction();
+                t.add(R.id.list, new AlbumsListFragment());
+                t.commit();
+
                 try {
                     mService.unregisterAlbumListCallback(albumListCallback);
                 } catch (RemoteException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                resetListAdapter();
             } else if (total != 0) { // Skip the very first result received.
                 try {
                     Log.v(TAG, "Calling albums(" + msg.arg2 + ")");
@@ -235,7 +217,10 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
 
     private final IServiceAlbumListCallback albumListCallback = new IServiceAlbumListCallback.Stub() {
         /**
-         * Update the affected rows.
+         * Process albums returned by the Squeezer Server.
+         * <p>
+         * Sends a message to the progress dialog handler so that it can update
+         * the dialog and fetch the next set of albums.
          *
          * @param count Number of items as reported by squeezeserver.
          * @param start The start position of items in this update.
@@ -247,7 +232,7 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
             Message msg = mProgressHandler.obtainMessage();
             msg.arg1 = start;
             msg.arg2 = (page + 1) * mPageSize; // The next page of albums to
-                                               // fetch
+                                               // fetch.
             mProgressHandler.sendMessage(msg);
         }
 
@@ -264,7 +249,7 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
             case PROGRESS_DIALOG:
                 progressDialog = new ProgressDialog(this);
                 progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-                progressDialog.setMessage("Loading...");
+                progressDialog.setMessage("Loading..."); // TODO: Localise.
                 return progressDialog;
             default:
                 return null;
@@ -283,42 +268,6 @@ public class AlbumsListActivity extends ListActivity implements AbsListView.OnSc
                     e.printStackTrace();
                 }
                 break;
-        }
-    }
-
-    /** OnItemClickListener methods */
-
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Uri albumUri = ContentUris
-                .withAppendedId(AlbumCache.Albums.CONTENT_ID_URI_BASE, id);
-        Intent i = new Intent(Intent.ACTION_VIEW, albumUri);
-        startActivity(i);
-    }
-
-    /** OnScrollListener methods */
-
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
-            int totalItemCount) {
-    }
-
-    public void onScrollStateChanged(AbsListView view, int scrollState) {
-        switch (scrollState) {
-            case OnScrollListener.SCROLL_STATE_IDLE:
-                mCursor.respond(LiveUpdateT);
-
-                Bundle extras = new Bundle();
-                extras.putInt("TYPE", AlbumCacheCursor.TYPE_REQUEST_PAGE);
-                extras.putInt("firstPosition", view.getFirstVisiblePosition());
-                extras.putInt("lastPosition", view.getLastVisiblePosition());
-                mCursor.respond(extras);
-
-                break;
-
-            case OnScrollListener.SCROLL_STATE_TOUCH_SCROLL:
-                break;
-
-            case OnScrollListener.SCROLL_STATE_FLING:
-                mCursor.respond(LiveUpdateF);
         }
     }
 }
