ac3b69283fc6b8f539fad7d9a278688c62d097bf
Nik Clayton <nik@ngo.org.uk>
2014-11-09 18:27:31 +0000

    Provide a mechanism to show licenses for included software.
    - Use de.psdev.licensesdialog:licensesdialog to show the license, which
      obsoletes LicenseDialog.java.
    
    - Provide tentative text for the Crashlytics license.

--- Squeezer/src/main/java/uk/org/ngo/squeezer/dialog/AboutDialog.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/dialog/AboutDialog.java
@@ -20,21 +20,29 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.View;
 import android.widget.TextView;
 
 import de.cketti.library.changelog.ChangeLog;
+import de.psdev.licensesdialog.LicensesDialog;
+import de.psdev.licensesdialog.licenses.ApacheSoftwareLicense20;
+import de.psdev.licensesdialog.licenses.License;
+import de.psdev.licensesdialog.model.Notice;
+import de.psdev.licensesdialog.model.Notices;
 import uk.org.ngo.squeezer.R;
 
 public class AboutDialog extends DialogFragment {
 
     @Override
+    @NonNull
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         @SuppressLint({"InflateParams"}) // OK, as view is passed to AlertDialog.Builder.setView()
         final View view = getActivity().getLayoutInflater().inflate(R.layout.about_dialog, null);
@@ -62,10 +70,64 @@ public class AboutDialog extends DialogFragment {
         });
         builder.setNegativeButton(R.string.dialog_license, new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                new LicenseDialog()
-                        .show(getActivity().getSupportFragmentManager(), "LicenseDialog");
+                final Notices notices = new Notices();
+                notices.addNotice(new Notice(
+                        "Squeezer",
+                        "https://github.com/nikclayton/android-squeezer",
+                        "Copyright (c) 2009 The Squeezer Authors",
+                        new ApacheSoftwareLicense20()));
+                notices.addNotice(new Notice(
+                        "Guava",
+                        "https://github.com/google/guava",
+                        "The Guava Authors",
+                        new ApacheSoftwareLicense20()));
+                notices.addNotice(new Notice(
+                        "ckChangeLog",
+                        "https://github.com/cketti/ckChangeLog",
+                        "Copyright (c) 2013 cketti",
+                        new ApacheSoftwareLicense20()));
+                notices.addNotice(new Notice(
+                        "Crashlytics",
+                        "http://www.crashlytics.com",
+                        "Copyright (c) 2014 Crashlytics",
+                        new CrashlyticsSoftwareLicense()));
+                LicensesDialog.Builder builder = new LicensesDialog.Builder(getActivity());
+
+                builder.setNotices(notices)
+                        .setIncludeOwnLicense(true)
+                        .setShowFullLicenseText(false)
+                        .build()
+                        .show();
             }
         });
         return builder.create();
+    }
+
+    private class CrashlyticsSoftwareLicense extends License {
+
+        private static final long serialVersionUID = 2260887278518124969L;
+
+        @Override
+        public String getName() {
+            return "Crashlytics Software License";
+        }
+
+        public String getSummaryText(Context context) {
+            return getContent(context, R.raw.crashlytics_summary);
+        }
+
+        public String getFullText(Context context) {
+            return getSummaryText(context);
+        }
+
+        @Override
+        public String getVersion() {
+            return "2014-07-15";
+        }
+
+        @Override
+        public String getUrl() {
+            return "http://try.crashlytics.com/terms/";
+        }
     }
 }
