e7bb2b0384f9ecd196e8e4885cf236126d41def0
Nik Clayton <nik@ngo.org.uk>
2018-09-18 10:50:01 +0200

    Don't show the menu item to go to PlayerListActivity if in PlayerListActivity.

--- Squeezer/src/main/java/uk/org/ngo/squeezer/NowPlayingFragment.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/NowPlayingFragment.java
@@ -976,6 +976,12 @@ public class NowPlayingFragment extends Fragment implements View.OnCreateContext
         if (mActivity instanceof CurrentPlaylistActivity && menu_item_playlist != null) {
             menu_item_playlist.setVisible(false);
         }
+
+        // Don't show the item to go to PlayerListActivity if in PlayerListActivity.
+        if (mActivity instanceof PlayerListActivity && menu_item_players != null) {
+            menu_item_players.setVisible(false);
+        }
+
         // Don't show the item to go to alarms if in AlarmsActivity.
         if (mActivity instanceof AlarmsActivity && menu_item_alarm != null) {
             menu_item_alarm.setVisible(false);
